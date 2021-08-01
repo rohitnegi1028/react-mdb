f74c06b99a2b092dd740cb15b8acd1349ae7a0b4
Rohit Negi <rohitnegi1028@gmail.com>
2021-08-01 15:07:39 +0530

    prop passoing with Hero componenet

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,12 +7,28 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 import NoImage from '../images/no_image.jpg';
 //Hooks
 import { useHomeFetch  } from '../hooks/useHomeFetch';
+import HeroImage from './HeroImage';
 
 
 const Home = () => {
     const {state, loading, error} = useHomeFetch();
-  
-    return (<div>Home Page</div>);
+    const getMovieIndex = (min, max) => { 
+        return Math.random() * (max - min) + min;
+    } 
+
+     
+    //Fragment shorthanded
+    return (<>
+        {/* {state.results[0] && */}
+        {state.results[0] ?
+        <HeroImage
+            image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[1].backdrop_path}`}
+            title={state.results[1].original_title}
+            text={state.results[1].overview}
+        />
+        : null
+        }
+    </>);
 
 }
 
