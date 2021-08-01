473d0e7c05dd9d85774f3e77be0eab1a0e0997ea
Rohit Negi <rohitnegi1028@gmail.com>
2021-08-01 20:37:15 +0530

    Routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
+
 import { GlobalStyle } from './GlobalStyle';
+
 import Header from './components/Header'; 
 import Home from './components/Home'; 
+import Movie from './components/Movie'; 
+import NotFound from './components/NotFound'; 
+
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 const App = () => (
-  <div className='App'>
+  <Router>
     <Header />
-    <Home />
+    <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/:movieId' element={<Movie/>} />
+        <Route path='/*' element={<NotFound/>} />
+
+    </Routes>
+    {/* <Home />  */}
     <GlobalStyle />
-  </div>
+  </Router>
 );
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -35,6 +35,7 @@ const Home = () => {
                 key={movie.id}
                 image={movie.poster_path ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path : NoImage}
                 movieId={movie.id}
+                clickable={true}
                 />
             ))
             }
